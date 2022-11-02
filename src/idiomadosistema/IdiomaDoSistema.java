ec8514ae7e7a299f7c26f7bb12610e4f37ced38d
Lidiany Caetano Gonçalves <lidiaany.caetano@gmail.com>
2022-11-02 18:00:49 -0300

    Adicionando arquivos de aulas Java

---
+++ src/idiomadosistema/IdiomaDoSistema.java
@@ -0,0 +1,15 @@
+
+package idiomadosistema;
+
+import java.util.Date;
+import java.util.Locale;
+
+
+public class IdiomaDoSistema {
+ public static void main(String[] args) {
+        Locale loc = Locale.getDefault();
+        System.out.print("O idioma do sistema é ");
+        System.out.println (loc.getDisplayLanguage());
+    }
+
+}
---
+++ src/resolucaodatela/ResolucaoDaTela.java
@@ -0,0 +1,15 @@
+
+package resolucaodatela;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class ResolucaoDaTela {
+
+    public static void main(String[] args) {
+     Toolkit tk = Toolkit.getDefaultToolkit();
+     Dimension d = tk.getScreenSize();
+     System.out.println("A resolução do seu sistema é: " + d.width +" X " + d.height);    
+    }
+    
+}
